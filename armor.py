f5d0b86f95f34712b94945ad732f029d37bfeae8
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-12 15:52:18 -0500

    built out armor class

--- armor.py
+++ armor.py
@@ -1,9 +1,24 @@
+import random
+
 class Armor:
     def __init__(self, name, max_block) -> None:
         """
         Takes in name: String, max_block: Integer as parameters
         """
-        pass
+        self.name = name
+        self.max_block = max_block
 
-    def attack(self):
-        pass
+    def block(self):
+        """
+        Return a block value that is randomly generated between 0 and max_damage. 
+        """
+        block_value = random.randint(0, self.max_block)
+        return block_value
+
+
+if __name__ == "__main__":
+  # If you run this file from the terminal
+  # this block is executed.
+  armor = Armor("Debugging Shield", 10)
+  print(armor.name)
+  print(armor.block())
