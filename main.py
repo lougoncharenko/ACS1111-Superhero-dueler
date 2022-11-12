67d2a6001d3ad8bc7929bd3c9f6497c053656ef9
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-12 15:45:49 -0500

    imported hero and ability class into main file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from ability import Ability
+from hero import Hero
 
+if __name__ == "__main__":
+    wonder_woman = Hero('Wonder Woman', 200)
+    hulk = Hero('Hulk', 200)
+    wonder_woman.fight(hulk)
+    ability = Ability("Testing Ability", 20)
+    print(ability.name)
+    print(ability.attack())
