3f85d0c08672c4ae3cfeadb045d78c6157c18e9d
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-12 15:47:46 -0500

    moved armor class into seperate file from hero

---
+++ armor.py
@@ -0,0 +1,9 @@
+class Armor:
+    def __init__(self, name, max_block) -> None:
+        """
+        Takes in name: String, max_block: Integer as parameters
+        """
+        pass
+
+    def attack(self):
+        pass
