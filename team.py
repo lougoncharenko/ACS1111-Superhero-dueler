6c54d832953141279d134f2bcf619a5e75e896c5
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-13 18:05:00 -0500

    initialized team class

---
+++ team.py
@@ -0,0 +1,13 @@
+class Team ():
+    def __init__(self, name):
+        self.name = name
+        self.heroes = list()
+
+    def add_hero(self, hero):
+        pass
+
+    def remove_hero(self, name):
+        pass
+    
+    def view_all_heroes():
+        pass
