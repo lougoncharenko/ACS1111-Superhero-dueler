a03291e577c69b7d0e1b1c2b330c0d4b24b71e92
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-13 18:17:47 -0500

    built out methods in team class

--- team.py
+++ team.py
@@ -1,13 +1,28 @@
 class Team ():
+    """
+    Team class does not inherit hero, rather contains Hero objects
+    This concept is known as aggregation in OOP
+    Initialize your team with its team name and an empty list of heroes
+    """
     def __init__(self, name):
         self.name = name
         self.heroes = list()
 
     def add_hero(self, hero):
-        pass
+        self.heroes.append(hero)
 
     def remove_hero(self, name):
-        pass
+        '''
+        Remove hero from heroes list.
+        If Hero isn't found return 0.
+        '''
+        for hero in self.heroes:
+            if hero.name == name:
+                self.heroes.remove(hero)
+            else: 
+                print(f'{name} does not exist on this team')
     
-    def view_all_heroes():
-        pass
+    def view_all_heroes(self):
+        '''Prints out all heroes to the console.'''
+        for hero in self.heroes:
+            print(hero.name)
