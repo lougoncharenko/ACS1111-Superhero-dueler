137ac20c90416bb024bf65a96a1cc58405384b54
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-13 15:19:25 -0500

    initialized weapons class that extends from ability classs

---
+++ weapon.py
@@ -0,0 +1,9 @@
+from ability import Ability
+import random
+
+class Weapon(Ability):
+    """ 
+    This method returns a random value
+    between one half to the full attack power of the weapon.
+    """
+    pass 
