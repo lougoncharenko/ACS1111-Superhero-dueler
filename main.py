c4368dec661610f78bf7f46be8ba24e77493c661
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-13 15:10:33 -0500

    called instances to test

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ from ability import Ability
 from armor import Armor
 from hero import Hero
 
+
+def battle_arena():
+    """
+    a while loop that keeps fight until one player returns false for is_alive
+    """
+    pass
+
 if __name__ == "__main__":
-    wonder_woman = Hero('Wonder Woman', 200)
+
+    # wonder woman
+    captain_america = Hero('Captain America', 200)
+    agility = Ability("Agility", 50)
+    captain_america.add_ability(agility)
+    shield = Armor("Captain America Sheild", 10)
+    captain_america.add_armor(shield)
+
+    # Hulk
     hulk = Hero('Hulk', 200)
-    wonder_woman.fight(hulk)
-    ability = Ability("Great Debugging", 50)
-    wonder_woman.add_ability(ability)
-    print(wonder_woman.attack())
-    armor = Armor("Debugging Shield", 10)
-    wonder_woman.add_armor(armor)
-    print(wonder_woman.defend())
+    hulk_smash=Ability('Hulk Smash', 50)
+    hulk.add_ability(hulk_smash)
+    mutated_skin = Armor('Mutated Skin', 10)
+    hulk.add_armor(mutated_skin)
+
+
+
+    # fight
+    captain_america.fight(hulk)
