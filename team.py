fa349d3528eb69f7b6c7541571ead388357f0e43
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-14 18:20:39 -0500

    building out team class

--- team.py
+++ team.py
@@ -29,3 +29,29 @@ class Team ():
         '''Prints out all heroes to the console.'''
         for hero in self.heroes:
             print(hero.name)
+
+    def stats(self):
+        '''Print team statistics'''
+        for hero in self.heroes:
+            kd = hero.kills / hero.deaths
+            print(f"{hero.name} Kill/Deaths:{kd}")
+
+    def revive_heroes(self, health=100):
+        ''' Reset all heroes health to starting_health'''
+        # TODO: for each hero in self.heroes,
+        # set the hero's current_health to their starting_health
+        for hero in self.heroes:
+            hero.current_health = hero.starting_health
+
+    def attack(self, other_team):
+        ''' Battle each team against each other.'''
+        living_heroes = list()
+        living_opponents = list()
+        for hero in self.heroes:
+            living_heroes.append(hero)
+
+        for hero in other_team.heroes:
+            living_opponents.append(hero)
+
+        while len(living_heroes) > 0 and len(living_opponents)> 0:
+            pass       
